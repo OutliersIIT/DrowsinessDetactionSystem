815f86e3a0f9a79b85f84f7eec47c6a3728af8ab
Sandeepa Perera <sandeepa.2017184@iit.ac.lk>
2021-02-23 21:11:45 +0530

    23-21 - normal pages

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ app.get("/about", function(req, res){
     res.render("about");
 })
 
+//get rout for the heart Rate
+app.get("/hartrate", function(req, res){
+    res.render("hartrate");
+})
+
+//get rout for the hartrate
+app.get("/activelevel", function(req, res){
+    res.render("activelevel")
+})
+
+//get rout for the hand motion
+app.get("/handmotion", function(req, res){
+    res.render("handmotion")
+})
+
 //app is listening to the port 3000 as server
 app.listen(port, function(){
    console.log("[Server] - Server is running on :/localhost/3000"); 
