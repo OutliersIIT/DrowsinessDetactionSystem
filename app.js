969fe60e7daf046e9a5f4e3e123dfa0f81182a57
Sandeepa Perera <sandeepa.2017184@iit.ac.lk>
2021-04-18 21:17:25 +0530

    18-04-2021 - commit

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 const port = 3000;
 
+//get rout for the app
+app.get("/outliers",function(req,res){
+    res.render("outliers")
+})
+
 //get route for the app
 app.get("/", function(req, res){
     res.render("home");
